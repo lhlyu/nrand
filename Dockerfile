5c1d8364a60dfbbde8704590a0f405d0ff0305bf
lhlyu <2438211370@qq.com>
2020-05-20 17:19:30 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
 FROM alpine:latest
 MAINTAINER "lhlyu"
 WORKDIR /app
-RUN mkdir conf && mkdir log && chmod 777 -R /app
+RUN mkdir conf && mkdir log
 COPY main ./main
 COPY conf/config.yaml conf/config.yaml
+COPY static static
+RUN chmod 777 -R /app
 CMD ["./main"]
 
 # 创建容器运行
-# docker build -t libra .
-# docker run -itd -p 9111:8080 -v /logs/libra-log:/app/log libra
+# docker build -t nrand .
+# docker run -itd -p 9003:8080 -v /logs/nrand-log:/app/log --name gen-name nrand
--- service/index_service.go
+++ service/index_service.go
@@ -4,6 +4,8 @@ import (
 	"github.com/lhlyu/nrand/controller/dto"
 	"github.com/lhlyu/nrand/service/gen"
 	"github.com/lhlyu/nrand/trace"
+	"github.com/lhlyu/yutil/v2"
+	"github.com/pkg/errors"
 )
 
 type IndexService struct {
@@ -19,6 +21,7 @@ func NewIndexService(tracker trace.ITracker) *IndexService {
 func (s *IndexService) GenName(req *dto.NameDto) []string {
 	g := gen.GenFactory(req.Target)
 	if g == nil {
+		s.Error(errors.New("not found gen"), yutil.Json.Marshal(req))
 		return nil
 	}
 	return g.Handler(req)
